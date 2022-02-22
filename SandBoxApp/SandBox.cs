9a1c2ad8e5cb79a88bac778cad5f7e83aecbf2c0
AdedayoDeveloper <olaleye.dedayo@gmail.com>
2022-02-22 18:48:01 +0000

    first-commit

---
+++ SandBoxApp/SandBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandBoxApp
+{
+    class SandBox : MarshalByRefObject
+    {
+        const string pathToUntrusted = @"..\..\..\UntrustedProject\bin\Debug";
+        const string untrustedAssembly = "UntrustedProject";
+        const string untrustedClass = "SandBoxApp.UntrustedProject.Program";
+        const string entryPoint = "IsFibonacci";
+        private static Object[] parameters = { 45 };
+        static void initializeSandBox()
+        {
+            AppDomainSetup appDomain = new AppDomainSetup();
+            appDomain.ApplicationBase = Path.GetFullPath(pathToUntrusted);
+        }
+       
+    }
+}
